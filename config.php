8140121b12c8e3bbdfbd96c1e252b450b06dd933
Nguyen42012685 <166701780+Nguyen42012685@users.noreply.github.com>
2025-05-18 18:25:06 +0200

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');      
-define('DB_NAME', 'zooriddle');           // Your database name
-define('DB_USER', 'root');                // Your DB username
-define('DB_PASS', '');            // Your DB password
-?>
