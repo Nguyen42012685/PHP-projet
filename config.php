ef8e1fce3816e5f7abd6b884ff406b1cca9d9585
Nguyen42012685 <166701780+Nguyen42012685@users.noreply.github.com>
2025-05-18 18:25:29 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'localhost');      
+define('DB_NAME', 'zooriddle');           // Your database name
+define('DB_USER', 'root');                // Your DB username
+define('DB_PASS', 'cobeo123');            // Your DB password
+?>
